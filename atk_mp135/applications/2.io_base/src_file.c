1de50a6ed9ff8737e4dd160d3bfb67a01f7c1284
Baotou1 <a625980798z@qq.com>
2025-05-19 10:25:47 +0800

    更新

---
+++ atk_mp135/applications/2.io_base/src_file.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc ,char *argv[])
+{
+    printf("111111111111111111\r\n");
+    printf("222222222222222222\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("333333333333333333\r\n");
+    printf("444444444444444444\r\n");
+    printf("444444444444444444\r\n");
+    printf("444444444444444444\r\n");
+    printf("444444444444444444\r\n");
+    printf("444444444444444444\r\n");
+    printf("444444444444444444\r\n");
+    printf("555555555555555555\r\n");
+    printf("555555555555555555\r\n");
+    printf("555555555555555555\r\n");
+    printf("555555555555555555\r\n");
+    return 0;
+}
